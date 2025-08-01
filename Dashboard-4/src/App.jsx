c598118e5b44fec7c47dc936aef9729de37d8ed4
manunaik128 <manumn2005@gmail.com>
2025-08-01 12:56:37 +0530

    third commit

--- Dashboard-4/src/App.jsx
+++ Dashboard-4/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './component/navbar/Navbar'
 import Content1 from './component/content1/Content1'
 import Content2 from './component/Content2/Content2'
+import Content3 from './component/Content3/Content3'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
     <Navbar/>
     <Content1/>
     <Content2/>
+    <Content3/>
       
     </div>
   )
---
+++ Dashboard-4/src/component/Content3/Content3.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './Content3.css'
+
+const Content3 = () => {
+    return (
+        <>
+            <div>
+                <div className="content3">
+                    <div className="options">
+                        <ul>
+                            <li><a href="">My request</a></li>
+                            <li><a href="">My Approval</a></li>
+                            <li><a href="">Recently prossed Request</a></li>
+                            <li><a href="">All Processed</a></li>
+
+                        </ul>
+
+
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Contract Name</th>
+                                    <th>Borrerer Name</th>
+                                    <th>Request Type</th>
+                                    <th>Due Date</th>
+                                    <th>Request Status</th>
+                                   
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Loan Agreement - NGEN</td>
+                                    <td>NextGen Enterprise</td>
+                                    <td>Review</td>
+                                    <td>27th Aug 2019</td>
+                                     <td><button>ko</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Loan Agreement - Game</td>
+                                    <td>Game Technology</td>
+                                    <td>Approval</td>
+                                    <td>20th Aug 2019</td>
+                                     <td><button>ko</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Loan Agreement - Tag</td>
+                                    <td>TAG Corp</td>
+                                    <td>Review</td>
+                                    <td>28th Jul 2019</td>
+                                     <td><button>ko</button></td>
+                                </tr>
+                                <tr>
+                                    <td>Loan Agreement - New LLC</td>
+                                    <td>New LLC</td>
+                                    <td>Sign - off</td>
+                                    <td>10th Jul 2019</td>
+                                     <td><button>ko</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+
+
+
+                </div>
+
+
+            </div>
+
+
+
+        </>
+    )
+}
+
+export default Content3
